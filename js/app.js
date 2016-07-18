3ed11bc7fec443844ad24ebc1f55ecc314a78d83
jingwen <jingwg@uw.edu>
2016-07-17 19:22:39 -0700

    Add list modal

--- js/app.js
+++ js/app.js
@@ -19,7 +19,8 @@ var myApp = angular.module('RecipeApp', ['ngSanitize', 'ui.router', 'ui.bootstra
 				 //The order list page 
   				.state('detail', {
 					  url:'/detail',
-   					 templateUrl: 'partials/detail.html' 
+   					 templateUrl: 'partials/detail.html' ,
+						controller:"JingwenDetailCtrl"
 				 })
 				 //The abstract list page
         		.state('lists', {
@@ -79,27 +80,56 @@ myApp.controller('recipiesSearch', ['$scope', '$http', function ($scope, $http) 
 }]);
 
 
+//The controller I designed for the button to show the modal
+myApp.controller('JingwenDetailCtrl',['$scope', '$http','$uibModal','ListService',function($scope,$http,$uibModal,ListService){
+	$scope.lists = ListService.lists;
+	//show modal
+	$scope.showListModal = function(){
+	var modalInstance = $uibModal.open(
+		{ templateUrl: 'partials/select-list-modal.html',
+		controller:'ModalCtrl',
+		scope:$scope // pass in all the scope variables
+		})
+	modalInstance.result.then(function(selectedItem) {
+				//put item on the scope!
+				$scope.list = selectedItem;
+				console.log("Now selected: ", $scope.list);
+			});
+	
+	}//end of show Modal
+}])
 
-    //add the recipe from the detail controller
+//the controller for the modal display all the lists for user to choose
+myApp.controller('ModalCtrl', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
+	//display the lists
+	$scope.select = function(list){
+		$scope.searchList = list;
+		//console.log('You selected', movie);
+	}
+	
+	//add recipe to the selected list
+	
+	
+	//close the modal
+	$scope.cancel = function() {
+		$uibModalInstance.dismiss('cancel');
+	};
 
-    //the list controller, let user interact will all the lists
+}]);
+
+//the list controller, let user interact will all the lists
     myApp.controller('ListCtrl',["$scope","ListService", function($scope, ListService){
 		$scope.lists = ListService.lists; // list is an array [list1, list2, list3], list1 = {name:favorite, content: [recipeA, recipeB, recipeC]}
 
         //delete specific list
-		$scope.deleteList= function(){
+		$scope.deleteList= function(listName){
+			console.log(listName);
 			ListService.deleteList(listName);
 			$scope.lists = ListService.lists;
 			alert("You successfuly deleted that!")
 		}
-		
-        //delete specific recipe object
-		$scope.removeRecipe = function(recipe){
-		    //find the list 
-            //find that recipe and then delete			
-		};
 
-        //create new ListCtrl
+        //create new List
         $scope.addList = function (listName) {
            var  newList = {};
            newList.name = $scope.listName;
@@ -107,18 +137,9 @@ myApp.controller('recipiesSearch', ['$scope', '$http', function ($scope, $http) 
         ListService.addList(newList);
         }
 
-		//show the list detail
-		 $scope.showListDetail = function(list){
-      	$scope.list = list;
-		  $scope.content = list.content;
-		  console.log("list.content")
-		  console.log(list.content[0]);
-
-    	}
-
 	}])
 
-
+//The controllerlet the listDetail page diplays all the recipe in the recipe list
 myApp.controller('ListDetailCtrl',["$scope","$stateParams","ListService","$filter", function($scope,$stateParams, ListService, $filter){
 	var lists = ListService.lists;
 	var targetList = [];
@@ -137,11 +158,9 @@ myApp.controller('ListDetailCtrl',["$scope","$stateParams","ListService","$filte
 myApp.factory('ListService', function(){
 		var service = {};
 		service.lists = [];
-		//just for testing
-		var exmapleRecipe = {name:"pizza",ingredients:["bananas","cocoa powder","peanut butter","almond milk"]};
-		var testList = {name:"favorite", content:[exmapleRecipe]};
+		//The defualt list
+		var testList = {name:"favorite", content:[]};
 		service.lists.push(testList);
-		console.log(service.lists);
 			
 	
 		//add recipe from the detail page to specific list
@@ -158,7 +177,7 @@ myApp.factory('ListService', function(){
             }
 	    };
 
-        //add a new List 
+        //add a new List into Lists
         service.addList = function(newlist) {
             service.lists.push(newlist);
 			console.log("Add new List");
