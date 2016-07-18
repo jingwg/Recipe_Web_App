bba3d39171a2fb06f7ee8c16da65c05efb55906e
Anirudh <your-email-addressanirudhr@uw.edugit config --global user.name your-full-nameAnirudh Ramanathan>
2016-07-18 14:25:22 -0700

    getting closer

--- js/app.js
+++ js/app.js
@@ -60,6 +60,7 @@ var myApp = angular.module('RecipeApp', ['ngSanitize', 'ui.router', 'ui.bootstra
 
 
 myApp.controller('recipiesSearch', ['$scope', '$http', function ($scope, $http) {
+	$scope.category = "";
 	$http.get('http://api.yummly.com/v1/api/recipes?_app_id=727f9e61&_app_key=6432cf347203b199cad6e4ccd21ba822&q=chicken').then(function (response) {
 		var data = response.data;
 		//do something with the data from the response...
@@ -75,7 +76,7 @@ myApp.controller('recipiesSearch', ['$scope', '$http', function ($scope, $http) 
 		//do something with the data from the response...
 		//like put it on the $scope to show it in the view!
 		$scope.items = data.matches;
-		console.log($scope.items);
+		//console.log($scope.items.attribute.course[0]);
 		//console.log($scope.items[0].recipeName)
 	});
 	};
