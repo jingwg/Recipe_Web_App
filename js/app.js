ae7c9b1607d70ec194ce465600a13d6f7ba1d8e2
eric ma <ericma000@gmail.com>
2016-07-18 14:25:28 -0700

    add homt.html

--- js/app.js
+++ js/app.js
@@ -3,45 +3,55 @@
 
 var myApp = angular.module('RecipeApp', ['ngSanitize', 'ui.router', 'ui.bootstrap']);
 
-	myApp.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-		
-			$stateProvider
-				//The home page
-				.state('home', {
-					url: '/', 
-					templateUrl: 'partials/home.html' 
-				})
-				//The abstract parent page
-				.state('category', {
-					url:'/vategory',
-					templateUrl: 'partials/category.html' 
-				})
-				 //The order list page 
-  				.state('detail', {
-					  url:'/detail',
-   					 templateUrl: 'partials/detail.html' 
-				 })
-				 //The shopping cart page
-        		.state('list', {
-					url:'/list',
-					templateUrl: 'partials/list.html' //path of the partial to load
-				})
-				//the detail Page
-				.state('signIn', {
-					url:'/signIn',
-					templateUrl: 'partials/signIn.html'//path of the partial to load
-				})
-				//$urlRouterProvider.otherwise('/');
-				//the search Page
-				.state('search', {
-					url:'/search',
-					templateUrl: 'partials/search.html'//path of the partial to load
-				})
-				$urlRouterProvider.otherwise('/');
-				
-	}]);
+myApp.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	
+		$stateProvider
+			//The home page
+			.state('home', {
+				url: '/', 
+				templateUrl: 'partials/home.html' 
+			})
+			//The abstract parent page
+			.state('category', {
+				url:'/vategory',
+				templateUrl: 'partials/category.html' 
+			})
+			 //The order list page 
+				.state('detail', {
+				  url:'/detail',
+					 templateUrl: 'partials/detail.html' 
+			 })
+			 //The shopping cart page
+    		.state('list', {
+				url:'/list',
+				templateUrl: 'partials/list.html' //path of the partial to load
+			})
+			//the detail Page
+			.state('signIn', {
+				url:'/signIn',
+				templateUrl: 'partials/signIn.html'//path of the partial to load
+			})
+			//$urlRouterProvider.otherwise('/');
+			//the search Page
+			.state('search', {
+				url:'/search',
+				templateUrl: 'partials/search.html'//path of the partial to load
+			})
+			$urlRouterProvider.otherwise('/');
+			
+}]);
 
 
+myApp.controller('FeatureCtrl', ['$scope', '$http', function ($scope, $http) {
+	$http.get('http://api.yummly.com/v1/api/recipes?_app_id=727f9e61&_app_key=6432cf347203b199cad6e4ccd21ba822&q=').then(function(response) {
+		$scope.message = "HELLO";
+		var data = response.data.matches;
+		$scope.topFour = _.sampleSize(data, 4);
+		console.log($scope.topFour);
+
+
+	});
+}]);
 
 
 myApp.controller('recipiesSearch', ['$scope', '$http', function ($scope, $http) {
